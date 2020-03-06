fb7d867b987dd8510c0fcc94eb397f448235e650
Lukashev <lnv161297@gmail.com>
2020-03-06 16:06:11 +0200

    Updates

---
+++ precache-manifest.2677c376886333df8eedf542d0f5e874.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032bcac2ee306fb0c2d2a724c1c77684",
+    "url": "/inmyroom_test/index.html"
+  },
+  {
+    "revision": "e655059aeb027180b394",
+    "url": "/inmyroom_test/static/css/main.a3267820.chunk.css"
+  },
+  {
+    "revision": "8b142fac0b54b9714d00",
+    "url": "/inmyroom_test/static/js/2.dfba0ebe.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/inmyroom_test/static/js/2.dfba0ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e655059aeb027180b394",
+    "url": "/inmyroom_test/static/js/main.c11877bd.chunk.js"
+  },
+  {
+    "revision": "bcb8ad2aa4a7c056b9a6",
+    "url": "/inmyroom_test/static/js/runtime-main.26f795a8.js"
+  },
+  {
+    "revision": "afea6c1e98cc2dbce8861c3852051bb8",
+    "url": "/inmyroom_test/static/media/basket.afea6c1e.svg"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/inmyroom_test/static/media/heart.8b0126c6.svg"
+  },
+  {
+    "revision": "f6e9a6f8baad6630d7c1854cfdd3ba58",
+    "url": "/inmyroom_test/static/media/product.f6e9a6f8.png"
+  }
+]);
