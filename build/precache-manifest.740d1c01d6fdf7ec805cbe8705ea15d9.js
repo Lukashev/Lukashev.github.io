ec0346b5fa39653e74702933c10ee08f24310d3d
Nikita <lnv161297@gmail.com>
2020-03-06 15:59:42 +0200

    add production build and edit .gitignore

---
+++ build/precache-manifest.740d1c01d6fdf7ec805cbe8705ea15d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6416ce14cb0dafe7293c477ed3a1263d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86eb7cd3535ec79b5128",
+    "url": "/static/css/main.a3267820.chunk.css"
+  },
+  {
+    "revision": "8b142fac0b54b9714d00",
+    "url": "/static/js/2.dfba0ebe.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.dfba0ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86eb7cd3535ec79b5128",
+    "url": "/static/js/main.a3d664fb.chunk.js"
+  },
+  {
+    "revision": "9245120990957e8cb5b5",
+    "url": "/static/js/runtime-main.bc8b9c81.js"
+  },
+  {
+    "revision": "afea6c1e98cc2dbce8861c3852051bb8",
+    "url": "/static/media/basket.afea6c1e.svg"
+  },
+  {
+    "revision": "8b0126c677112ff8ac1c581569efaae2",
+    "url": "/static/media/heart.8b0126c6.svg"
+  },
+  {
+    "revision": "f6e9a6f8baad6630d7c1854cfdd3ba58",
+    "url": "/static/media/product.f6e9a6f8.png"
+  }
+]);
